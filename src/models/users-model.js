cc604a5abf4a76f232bc31a9346a113e2647266a
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-04-24 23:28:44 +0300

    prepared query for UsersModel

--- src/models/users-model.js
+++ src/models/users-model.js
@@ -4,6 +4,7 @@
  */
 
 import dbConfig from '../db-config';
+const PQ = require('pg-promise').ParameterizedQuery;
 
 /** Class representing an Users model. */
 export default new class UsersModel {
@@ -22,12 +23,12 @@ export default new class UsersModel {
             data: null
         };
         try {
-            result.data = await this._dbContext.one(`INSERT INTO users (nickname, about, fullname, email) 
-            VALUES ('${nickname}', '${userData.about}', '${userData.fullname}', '${userData.email}') 
-            RETURNING *`);
+            const createUserQuery = new PQ(`INSERT INTO users (nickname, about, fullname, email) 
+                VALUES ($1, $2, $3, $4) RETURNING *`);
+            createUserQuery.values = [nickname, userData.about, userData.fullname, userData.email];
+            result.data = await this._dbContext.one(createUserQuery);
             result.isSuccess = true;
-        }
-        catch (error) {
+        } catch (error) {
             result.message = error.message;
             console.log('ERROR: ', error.message || error);
         }
@@ -36,31 +37,30 @@ export default new class UsersModel {
 
     async getUserByNicknameOrEmail(nickname, email) {
         try {
-            return await this._dbContext.manyOrNone(`SELECT * FROM users WHERE nickname = '${nickname}'
-            OR email = '${email}'`);
-        }
-        catch (error) {
+            const getUserQuery = new PQ(`SELECT * FROM users WHERE nickname = $1 OR email = $2`);
+            getUserQuery.values = [nickname, email];
+            return await this._dbContext.manyOrNone(getUserQuery);
+        } catch (error) {
             console.log('ERROR: ', error.message || error);
         }
     }
 
     async getUserByNickname(nickname) {
         try {
-            return await this._dbContext.oneOrNone(`SELECT * FROM users WHERE nickname = '${nickname}'`);
-        }
-        catch (error) {
+            const getUserQuery = new PQ(`SELECT * FROM users WHERE nickname = $1`, [nickname]);
+            return await this._dbContext.oneOrNone(getUserQuery);
+        } catch (error) {
             console.log('ERROR: ', error.message || error);
         }
     }
 
     async updateUser(nickname, userData) {
         try {
-            return await this._dbContext.oneOrNone(`UPDATE users SET
-            about = '${userData.about}', 
-            fullname = '${userData.fullname}', 
-            email = '${userData.email}'
-            WHERE nickname = '${nickname}'
-            RETURNING *`);
+            const updateUserQuery = new PQ(`UPDATE users SET
+                about = $1, fullname = $2, email = $3 WHERE nickname = $4 
+                RETURNING *`);
+            updateUserQuery.values = [userData.about, userData.fullname, userData.email, nickname];
+            return await this._dbContext.oneOrNone(updateUserQuery);
         }
         catch (error) {
             console.log('ERROR: ', error.message || error);
